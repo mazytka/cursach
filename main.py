62e824e022a5ef32924bdad4a8127f6afdc07052
mazytka <95392543+mazytka@users.noreply.github.com>
2023-06-17 20:28:49 +0300

    add services

--- main.py
+++ main.py
@@ -4,9 +4,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+SERVICES = [{
+    'id': 1,
+    'title': 'Стрижка',
+    'price': '15000 рублей'
+},
+           {
+    'id': 2,
+    'title': 'Педикюр',
+    'price': '1500 рублей'
+},
+{
+    'id': 3,
+    'title': 'Солярий',
+    'price': '25000 рублей'
+},
+{
+    'id': 4,
+    'title': 'Маникюр',
+    'price': '2000 рублей'
+}
+]
+
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', services=SERVICES)
 
 
 if __name__ == "__main__":
