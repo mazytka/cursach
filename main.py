9b3517a150e98b280d80c12395db6134af2f0739
mazytka <95392543+mazytka@users.noreply.github.com>
2023-06-19 02:15:40 +0300

    executed from db services

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
 from flask import Flask, render_template
+import database as db
 
 
 app = Flask(__name__)
 
 
-SERVICES = [{
-    'id': 1,
-    'title': 'Стрижка',
-    'price': '15000 рублей'
-},
-           {
-    'id': 2,
-    'title': 'Педикюр',
-    'price': '1500 рублей'
-},
-{
-    'id': 3,
-    'title': 'Солярий',
-    'price': '25000 рублей'
-},
-{
-    'id': 4,
-    'title': 'Маникюр',
-    'price': '2000 рублей'
-}
-]
-
 @app.route("/")
 def hello():
-    return render_template('home.html', services=SERVICES)
+    services = db.load_services_from_db()  # Берет функции из модуля database.py
+    return render_template('home.html', services=services)
 
 
 if __name__ == "__main__":
