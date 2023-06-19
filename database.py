bffdd4c5d54eb6eec22c3f3e87b7212894fabb63
mazytka <95392543+mazytka@users.noreply.github.com>
2023-06-19 05:00:18 +0300

    add databases func

--- database.py
+++ database.py
@@ -11,3 +11,24 @@ def load_services_from_db():
             services.append(row)
     return services
 
+
+def load_service_from_db(id):
+   with engine.connect() as conn:
+        result = conn.execute(text(f"SELECT * FROM service WHERE id={id}"))
+        rows = []
+        for row in result.all():
+            rows.append(row._mapping)
+        if len(rows) == 0:
+            return None
+        else:
+            return row
+
+
+def load_client_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text('select * from client'))
+        for i in result:
+            return i
+
+
+print(load_services_from_db())
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import database as db
 
 
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def hello():
     services = db.load_services_from_db()  # Берет функции из модуля database.py
     return render_template('home.html', services=services)
+
+@app.route("/<id>")
+def show_service(id):
+    service = db.load_service_from_db(id)
+    return render_template('servicepage.html', service=service)
 
 
 if __name__ == "__main__":
