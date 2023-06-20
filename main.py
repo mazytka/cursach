4e8d2ea113a45c6ea4ada9e85950be623bf6a9bc
mazytka <95392543+mazytka@users.noreply.github.com>
2023-06-20 13:47:49 +0300

    add new pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 import database as db
 
 
@@ -8,15 +8,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     services = db.load_services_from_db()  # Берет функции из модуля database.py
-    # min_price = db.load_min_price_from_service()
     return render_template('home.html', services=services)
+
+
 @app.route("/<id>")
 def show_service(id):
     service = db.load_service_from_db(id)
     servicec = db.load_service_price_from_db(id)
+    master = db.load_master_from_db(id)
     if not service:
         return "Not Found", 404
-    return render_template('servicepage.html', service=service, servicec = servicec)
+    return render_template('servicepage.html', service=service, servicec=servicec, master=master)
+
+
+@app.route("/<id>/apply", methods=['post'])
+def apply(id):
+    data = request.form
+    return render_template('application_submitted.html', data=data)
 
 
 if __name__ == "__main__":
