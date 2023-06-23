218ac54acca0409a11d8acff2a86653d7a4dac8c
mazytka <95392543+mazytka@users.noreply.github.com>
2023-06-23 19:58:32 +0300

    add register and login part 2

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flash, g, abort, redirect, url_for
 import database as db
-from flask_admin import Admin
-
+from UserLogin import UserLogin
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
+
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    print('load_user')
+    return UserLogin().fromDB(user_id, app)
 
 
 @app.route("/")
@@ -32,8 +40,42 @@ def apply(id):
     return render_template('application_submitted.html', data=data, master=master, service=service)
 
 
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = db.get_user_by_email(request.form['email'])
+        print(user.psw)
+        if user and check_password_hash(user.psw, request.form['psw']):
+            userlogin = UserLogin().create(user)
+            print(userlogin.get_id())
+            login_user(userlogin)
+            print(3)
+            return redirect(url_for('home'))
+        else:
+            flash('Неверная пара логин/пароль', 'error')
 
+    return render_template("login.html")
+
+
+@app.route("/register", methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        if request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = db.add_user(request.form['name'], request.form['email'], hash)
+            if res:
+                flash("Регистрация прошла успешно")
+                return redirect(url_for('login'))
+
+            else:
+                flash("Почтовый адрес уже зарегистрирован", 'error')
+        else:
+            flash('Пароли не совпадают', 'error')
+
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'static'
     app.run(debug=True)
